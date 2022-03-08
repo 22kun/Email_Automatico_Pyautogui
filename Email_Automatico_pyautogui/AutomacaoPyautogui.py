f965f5459abad343671c2324963689b2f0791043
Artur <97131923+22kun@users.noreply.github.com>
2022-03-07 23:47:34 -0300

    Email Automático usando Pyautogui

---
+++ Email_Automatico_pyautogui/AutomacaoPyautogui.py
@@ -0,0 +1,28 @@
+import pyautogui, pyperclip, time
+
+## Enviar email
+# Abrir navegador
+pyautogui.hotkey("Win")
+time.sleep(1)
+pyautogui.write("opera")
+pyautogui.press("enter")
+time.sleep(5)
+
+# Abrir email
+pyautogui.write("gmail.com")
+pyautogui.press("enter")
+time.sleep(7)
+
+# Escrever
+pyautogui.click(x=161, y=231, clicks=1)
+time.sleep(2)
+pyautogui.write("arturdemaria7@gmail.com")
+pyautogui.press("tab")
+
+pyautogui.press("tab")
+pyperclip.copy("Email Automático - Artur")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("tab")
+pyperclip.copy("Este é um email automático.")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("ctrl", "enter")
